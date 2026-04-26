51caa4a69126ca22e7684de4732266004d5b2bf7
Timur <timursultanov36@gmail.com>
2026-04-26 12:50:14 +0200

    Added Text Assignments and little adjustments

--- Assignment 1/Task1.py
+++ Assignment 1/Task1.py
@@ -1,32 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# read csv-File
+data = pd.read_csv("publ_years.csv")
 
-data = pd.read_csv("Assignment 1/publ_years.csv")
-print(data.head())
-
-# author span
+# Activity Span
 data['span'] = data['LAST_YEAR'] - data['FIRST_YEAR'] + 1
-print(data.head())
 
 # min
-print("min ",data['span'].min())
-# max
-print("max ",data['span'].max())
-# lower quartil - 25% see https://en.wikipedia.org/wiki/Quartile
-print("lower quartil ",data['span'].quantile(q=0.25))
-# upper quartile
-print("upper quartile ",data['span'].quantile(q=0.75))
-# median
-print("median ",data['span'].median())
-# mean
-print("mean ",data['span'].mean())
 
-#show
+print("min: ", data['span'].min())
+# max
+print("max: ", data['span'].max())
+# lower quartil
+print("lower quartil: ", data['span'].quantile(0.25))
+# upper quartile
+print("upper quartile: ", data['span'].quantile(0.75))
+# median
+print("median: ", data['span'].median())
+# mean
+print("mean: ", data['span'].mean())
+
+# show
 boxplot = data.boxplot(column=["span"])
 
-#without outliers
+# without outliers
 data.boxplot(column=["span"], showfliers=False)
 plt.show()
-
-
--- Assignment 1/Task2.py
+++ Assignment 1/Task2.py
@@ -1,43 +1,47 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+data = pd.read_csv("publ_years.csv")
 
-data = pd.read_csv("Assignment 1/publ_years.csv")
-print(data.head())
-
-# author span
+# Activity span
 data['span'] = data['LAST_YEAR'] - data['FIRST_YEAR'] + 1
-print(data.head())
 
-print(data['LAST_YEAR'].max())
-#show, 73 bins beacuse min is 0 and max is 72
-histogram = data.hist(column=["span",'LAST_YEAR','FIRST_YEAR'], bins=73)
-plt.show()
+# Example of each class that ís removed
+ex_invalid = data[data['span'] < 1].head(1)
+ex_long = data[data['span'] > 40].head(1)
+ex_old = data[(data['FIRST_YEAR'] < 1950) | (data['LAST_YEAR'] < 1950)].head(1)
+# Print examples
+print(f"Klasse span < 1:\n{ex_invalid[['#KEY', 'FIRST_YEAR', 'LAST_YEAR', 'span']]}\n")
+print(f"Klasse span > 40:\n{ex_long[['#KEY', 'FIRST_YEAR', 'LAST_YEAR', 'span']]}\n")
+print(f"Klasse vor 1950:\n{ex_old[['#KEY', 'FIRST_YEAR', 'LAST_YEAR', 'span']]}")
 
-#remove spans less than 1 because LAST YEAR >= First YEAR, so your last publication cannot be at year n while your first year be n+1
-print("percentage of +40 year span",(data["span"] > 40).sum()/len(data))
+# Filter
+# Remove spans less than 1 because LAST YEAR >= First YEAR, so your last publication cannot be at year n while your first year be n+1
+print("percentage of +40 year span", (data["span"] > 40).sum() / len(data))
 print("amount to discrad", (data["span"] > 40).sum())
-print("percentage of less than 1 year span",(data["span"] < 1).sum()/len(data))
+print("percentage of less than 1 year span", (data["span"] < 1).sum() / len(data))
 print("amount to discrad", (data["span"] < 1).sum())
-print("percentage of erlier than 1950",(data["FIRST_YEAR"] < 1950).sum()/len(data))
+print("percentage of erlier than 1950", (data["FIRST_YEAR"] < 1950).sum() / len(data))
 print("amount to discrad", (data["FIRST_YEAR"] < 1950).sum())
-data = data[data["span"] <= 40] # remove long spans which as ther only make 0.4704314874258819 percent of the whole dataset
+data = data[
+    data["span"] <= 40]  # Remove long spans which as ther only make 0.4704314874258819 percent of the whole dataset
 data = data[data["span"] >= 1]
-data = data[data["LAST_YEAR"] >= 1950] # we consider computing timeline starting at 1950 https://en.wikipedia.org/wiki/Category:Computing_timelines 
+data = data[data[
+                "LAST_YEAR"] >= 1950]  # We consider computing timeline starting at 1950 https://en.wikipedia.org/wiki/Category:Computing_timelines
 data = data[data["FIRST_YEAR"] >= 1950]
 
-histogram = data.hist(column=["span",'LAST_YEAR','FIRST_YEAR'], bins=40)
+histogram = data.hist(column=["span", 'LAST_YEAR', 'FIRST_YEAR'], bins=40)
 plt.show()
 
 # min
-print("min ",data['span'].min())
+print("min ", data['span'].min())
 # max
-print("max ",data['span'].max())
-# lower quartil - 25% see https://en.wikipedia.org/wiki/Quartile
-print("lower quartil ",data['span'].quantile(q=0.25))
+print("max ", data['span'].max())
+# lower quartil
+print("lower quartil ", data['span'].quantile(0.25))
 # upper quartile
-print("upper quartile ",data['span'].quantile(q=0.75))
+print("upper quartile ", data['span'].quantile(0.75))
 # median
-print("median ",data['span'].median())
+print("median ", data['span'].median())
 # mean
-print("mean ",data['span'].mean())
+print("mean ", data['span'].mean())
